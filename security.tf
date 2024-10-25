db9c5567d729459735ace99e985954708cd3ed8f
Federico François <federico.francois@kiratech.it>
2024-10-26 01:51:20 +0200

    refactor: update security with variables

--- security.tf
+++ security.tf
@@ -1,24 +1,31 @@
 resource "aws_key_pair" "admin-kp" {
-  key_name   = "bastion-admin"
-  public_key = file("./key.pub")
+  key_name   = var.instance_key_pair.name
+  public_key = file(var.instance_key_pair.public_ssh_key)
 }
 
 resource "aws_security_group" "bastion-sg" {
   name   = "allow-all-sg"
   vpc_id = aws_vpc.bastion-vpc.id
-  ingress {
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+
+  dynamic "ingress" {
+    for_each = var.instance_ingress_rules
+
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = var.instance_egress_rules
+
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,46 @@
-variable "ssh_keys" {
-  type = list(string)
+variable "instance_key_pair" {
+  type = object({
+    name           = string
+    public_ssh_key = string
+  })
+  default = {
+    name           = "admin"
+    public_ssh_key = "./key.pub"
+  }
+}
 
-  default = ["./key"]
+variable "instance_ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+
+  default = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "instance_egress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 }
